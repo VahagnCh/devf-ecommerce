381250dbfe94f174d2b92d6aba5ba6b4e9a150b0
VahagnCh <vahagnchavushyan@gmail.com>
2024-07-16 12:45:19 -0600

    Single Product Pruebas

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,69 +1,68 @@
 import { useState } from 'react'
 import { StarIcon } from '@heroicons/react/20/solid'
 import { Radio, RadioGroup } from '@headlessui/react'
+import { ProductContext } from '../context/Productcontext'
 
-const product = {
+/*const product = {
   name: 'Basic Tee 6-Pack',
   brand: 'alguna',
   price: '$192',
   href: '#',
-  images: [
-    {
-      src: 'https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg',
-      alt: 'Two each of gray, white, and black shirts laying flat.',
-    },
+  images: [ {},
   ],
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
 }
-const reviews = { href: '#', average: 4, totalCount: 117 }
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
-}
+}*/
 
 export default function SingleProductPage() {
+    const { products } = useContext(ProductContext)
 
-  return (
-    <div className="bg-white">
-      <div className="pt-6">
-        
+    return (
+        <div className="bg-white">
+            <div className="pt-6">
+                {products.map((product) => (
+                    <div key={product.id} className="group relative">
+                        {/* Image gallery */}
+                        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+                            <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+                                <img
+                                    src={product.image}
+                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full rounded-2xl"
+                                />
+                            </div>
+                        </div>
 
-        {/* Image gallery */}
-        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-          <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-            <img
-              alt={product.images[0].alt}
-              src={product.images[0].src}
-              className="h-full w-full object-cover object-center"
-            />
-          </div>
-        </div>
+                        {/* Product info */}
+                        <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+                            <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                                <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
+                            </div>
 
-        {/* Product info */}
-        <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
-          </div>
+                            {/* Options */}
+                            <div className="mt-4 lg:row-span-3 lg:mt-0">
+                                <h2 className="sr-only">Product information</h2>
+                                <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
+                            </div>
 
-          {/* Options */}
-          <div className="mt-4 lg:row-span-3 lg:mt-0">
-            <h2 className="sr-only">Product information</h2>
-            <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
-          </div>
+                            <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+                                {/* Description and details */}
+                                <div>
+                                    <h3 className="sr-only">Description</h3>
 
-          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-            {/* Description and details */}
-            <div>
-              <h3 className="sr-only">Description</h3>
-
-              <div className="space-y-6">
-                <p className="text-base text-gray-900">{product.description}</p>
-              </div>
+                                    <div className="space-y-6">
+                                        <p className="text-base text-gray-900">{product.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
+
